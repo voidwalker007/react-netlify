8beca3eb688092f5e1ea209fc8c404151f67ff1d
akshay vishwakarma <66773987+voidwalker007@users.noreply.github.com>
2020-11-11 21:48:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+
+
+import { useEffect, useState } from 'react';
+
 import './App.css';
 
 function App() {
+
+ const [data, setData] = useState([]);
+ const [data1, setData1] = useState([]);
+
+
+ useEffect(() => {
+  fetchData();
+
+  loadData();
+ }, [])
+
+async function fetchData(){
+   const api="https://jsonplaceholder.typicode.com/users";
+
+   const result= await fetch(api);
+   const data1 = await result.json();
+   console.log(data1);
+   setData1(data1);
+
+ }
+
+ const loadData = async () =>{
+
+   await fetch("https://jsonplaceholder.typicode.com/users")
+  .then(response =>response.json())
+  .then( receiveddata => setData(receiveddata))
+  .catch(error =>{
+    console.log(error,"error aa gya");
+  })
+
+ }
+ 
+
+ 
   return (
+   
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+      
+       {data.map( (user)=>(
+         <div className="listv" key={user.id}>{user.name}</div>
+       ))}
+
+       <br/>
+       <hr/>-
+        {data1.map( (user1)=>(
+         <div className="listv" key={user1.id}>{user1.name}</div>
+       ))}
+       
     </div>
   );
 }
